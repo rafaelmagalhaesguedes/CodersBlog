7d56fb8bf77186dc39d9303b7a48d66e0d1ee966
rafael <rafaelmagalhaes1@live.com>
2024-03-16 20:40:12 -0300

    tests: added tests to categories findAll and create

--- tests/integration/categories.test.ts
+++ tests/integration/categories.test.ts
@@ -18,12 +18,12 @@ const tokenMock = jwt.sign(payload, secret, options);
 chai.use(chaiHttp);
 
 describe('Categories Tests', function () {
-  describe('Get all categories', function () {
+  //
+  afterEach(() => {
+    sinon.restore();
+  });
 
-    afterEach(() => {
-      sinon.restore();
-    });
-  
+  describe('Get all categories', function () {
     //
     it('should return all categories', async function () {
       // arrange
@@ -35,7 +35,8 @@ describe('Categories Tests', function () {
       sinon.stub(CategoriesModel.prototype, 'findAll').resolves(categoriesMock as any);
 
       // act
-      const res = await chai.request(app).get('/categories').set('Authorization', `Bearer ${tokenMock}`);
+      const res = await chai.request(app)
+        .get('/categories').set('Authorization', `Bearer ${tokenMock}`);
       
       // assert
       expect(res.status).to.equal(200);
@@ -48,10 +49,62 @@ describe('Categories Tests', function () {
       sinon.stub(CategoriesModel.prototype, 'findAll').resolves(null);
 
       // act
-      const res = await chai.request(app).get('/categories').set('Authorization', `Bearer ${tokenMock}`);
+      const res = await chai.request(app)
+        .get('/categories').set('Authorization', `Bearer ${tokenMock}`);
       
       // assert
       expect(res.status).to.equal(404);
+    });
+  });
+
+  describe('Create a category', function () {
+    //
+    it('should create a category', async function () {
+      // arrange
+      const categoryMock = { id: 1, name: 'Category 1' };
+
+      sinon.stub(CategoriesModel.prototype, 'findByName').resolves(null);
+      sinon.stub(CategoriesModel.prototype, 'create').resolves(categoryMock as any);
+
+      // act
+      const res = await chai.request(app)
+        .post('/categories').set('Authorization', `Bearer ${tokenMock}`)
+        .send({ name: 'Category 1' });
+      
+      // assert
+      expect(res.status).to.equal(201);
+      expect(res.body).to.deep.equal(categoryMock);
+    });
+
+    //
+    it('should return an error when trying to create a category', async function () {
+      // arrange
+      sinon.stub(CategoriesModel.prototype, 'findByName').resolves({ id: 1, name: 'Category 1' });
+
+      // act
+      const res = await chai.request(app)
+        .post('/categories').set('Authorization', `Bearer ${tokenMock}`)
+        .send({ name: 'Category 1' });
+      
+      // assert
+      expect(res.status).to.equal(409);
+      expect(res.body).to.deep.equal({ message: 'Category already exists!' });
+    });
+
+    //
+    it('should return an error when trying to create a category', async function () {
+      // arrange
+      sinon.stub(CategoriesModel.prototype, 'findByName').resolves(null);
+      sinon.stub(CategoriesModel.prototype, 'create').resolves(null);
+
+      // act
+      const res = await chai.request(app)
+        .post('/categories').set('Authorization', `Bearer ${tokenMock}`)
+        .send({ name: 'Category 1' });
+      
+      // assert
+      expect(res.status).to.equal(500);
+      expect(res.body).to.deep.equal({ message: 'Category not created!' });
     });
   });
 });
