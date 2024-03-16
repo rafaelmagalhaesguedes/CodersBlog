25ebfe9faba2ffb5a7c676cb7a36b0cfec8ac500
rafael <rafaelmagalhaes1@live.com>
2024-03-16 19:21:05 -0300

    tests: add tests to endpoint findAll categories

---
+++ tests/integration/categories.test.ts
@@ -0,0 +1,57 @@
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+import chaiHttp = require('chai-http');
+import { app } from '../../src/app';
+import * as jwt from 'jsonwebtoken';
+import CategoriesModel from '../../src/models/CategoriesModel';
+
+const { expect } = chai;
+
+// Options for the token
+const secret = process.env.JWT_SECRET || 'secretKey';
+const payload = { id: 1, email: 'rafael@dev.com'};
+const options = { expiresIn: '1h' };
+
+// Generate a token
+const tokenMock = jwt.sign(payload, secret, options);
+
+chai.use(chaiHttp);
+
+describe('Categories Tests', function () {
+  describe('Get all categories', function () {
+
+    afterEach(() => {
+      sinon.restore();
+    });
+  
+    //
+    it('should return all categories', async function () {
+      // arrange
+      const categoriesMock = [
+        { id: 1, name: 'Category 1' },
+        { id: 2, name: 'Category 2' }
+      ];
+
+      sinon.stub(CategoriesModel.prototype, 'findAll').resolves(categoriesMock as any);
+
+      // act
+      const res = await chai.request(app).get('/categories').set('Authorization', `Bearer ${tokenMock}`);
+      
+      // assert
+      expect(res.status).to.equal(200);
+      expect(res.body).to.deep.equal(categoriesMock);
+    });
+
+    //
+    it('should return an error when trying to get all categories', async function () {
+      // arrange
+      sinon.stub(CategoriesModel.prototype, 'findAll').resolves(null);
+
+      // act
+      const res = await chai.request(app).get('/categories').set('Authorization', `Bearer ${tokenMock}`);
+      
+      // assert
+      expect(res.status).to.equal(404);
+    });
+  });
+});
