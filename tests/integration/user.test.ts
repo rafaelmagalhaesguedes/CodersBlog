313bb820f0f7d78416a0d9a72321efc6f68f7c47
rafael <rafaelmagalhaes1@live.com>
2024-03-16 17:17:16 -0300

    tests: add tests to get user by id

--- tests/integration/user.test.ts
+++ tests/integration/user.test.ts
@@ -24,47 +24,81 @@ describe('User Tests', function () {
     sinon.restore();
   });
 
-  it('should create a new user', async function () {
-    // arrange
-    sinon.stub(UserModel.prototype, 'create').resolves(userMock as any);
+  describe('Create user', function () {
+    //
+    it('should create a new user', async function () {
+      // arrange
+      sinon.stub(UserModel.prototype, 'create').resolves(userMock as any);
 
-    // act
-    const res = await chai.request(app).post('/user').send(userMock);
-    
-    // assert
-    expect(res.status).to.equal(201);
+      // act
+      const res = await chai.request(app).post('/user').send(userMock);
+      
+      // assert
+      expect(res.status).to.equal(201);
+    });
+
+    //
+    it('should return an error when trying to create a user that already exists', async function () {
+      // arrange
+      sinon.stub(UserModel.prototype, 'getByEmail').resolves(userMock as any);
+
+      // act
+      const res = await chai.request(app).post('/user').send(userMock);
+      
+      // assert
+      expect(res.status).to.equal(409);
+    }); 
   });
 
-  it('should return all users', async function () {
-    // arrange
-    sinon.stub(UserModel.prototype, 'findAll').resolves(allUsersMock as any);
-    sinon.stub(jwt, 'verify').returns(payload as any);
+  describe('Get aLL users', function () {
+    //
+    it('should return all users', async function () {
+      // arrange
+      sinon.stub(UserModel.prototype, 'findAll').resolves(allUsersMock as any);
+      sinon.stub(jwt, 'verify').returns(payload as any);
 
-    // act
-    const res = await chai.request(app).get('/user').set('Authorization', `Bearer ${tokenMock}`);
-    
-    // assert
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an('array');
-    expect(res.body).to.deep.equal(allUsersMock);
+      // act
+      const res = await chai.request(app).get('/user').set('Authorization', `Bearer ${tokenMock}`);
+      
+      // assert
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('array');
+      expect(res.body).to.deep.equal(allUsersMock);
+    });
+
+    //
+    it('should return an error when trying to get all users without a token', async function () {
+      // act
+      const res = await chai.request(app).get('/user');
+      
+      // assert
+      expect(res.status).to.equal(401);
+    });
   });
 
-  it('should return an error when trying to create a user that already exists', async function () {
-    // arrange
-    sinon.stub(UserModel.prototype, 'getByEmail').resolves(userMock as any);
+  describe('Get user by id', function () {
+    //
+    it('should return a user by id', async function () {
+      // arrange
+      sinon.stub(UserModel.prototype, 'getById').resolves(userMock as any);
+      sinon.stub(jwt, 'verify').returns(payload as any);
 
-    // act
-    const res = await chai.request(app).post('/user').send(userMock);
-    
-    // assert
-    expect(res.status).to.equal(409);
-  });
+      // act
+      const res = await chai.request(app).get('/user/1').set('Authorization', `Bearer ${tokenMock}`);
+      
+      // assert
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('object');
+      expect(res.body).to.deep.equal(userMock);
+    });
 
-  it('should return an error when trying to get all users without a token', async function () {
-    // act
-    const res = await chai.request(app).get('/user');
-    
-    // assert
-    expect(res.status).to.equal(401);
+    //
+    it('should return an error when trying to get a user by id without a token', async function () {
+      // act
+      const res = await chai.request(app).get('/user/1');
+      
+      // assert
+      expect(res.status).to.equal(401);
+    });
   });
 });
