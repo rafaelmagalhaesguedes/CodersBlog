dc88ec7973e93f5778706e6d8651f4106dbeff5f
rafael <rafaelmagalhaes1@live.com>
2024-03-16 23:50:35 -0300

    fix: ICategoriesModel interface

--- src/interfaces/Categories/ICategoriesModel.ts
+++ src/interfaces/Categories/ICategoriesModel.ts
@@ -5,5 +5,6 @@ export interface ICategoriesModel {
   findAll: () => Promise<ICategories[] | null>;
   findById: (id: ICategories['id']) => Promise<ICategories | null>;
   findByName: (name: ICategories['name']) => Promise<ICategories | null>;
+  findCategory: (categoryIds: ICategories['id'][]) => Promise<boolean>;
   create: (category: ICategories) => Promise<ICategories | null>;
 }
