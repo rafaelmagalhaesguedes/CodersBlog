097aaead0c043a80ce2fc7af31c41691462b0b53
rafael <rafaelmagalhaes1@live.com>
2024-03-18 19:12:45 -0300

    fix: config Access Control on App /backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,8 +11,20 @@ class App {
   constructor() {
     this.app = express();
     this.mainRoutes = new MainRoutes();
-    this.app.use(express.json());
+    this.config();
     this.routes();
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
   }
 
   private routes(): void {
