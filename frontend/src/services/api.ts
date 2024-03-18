600be09b442d3715376c4afb54484484b837da84
rafael <rafaelmagalhaes1@live.com>
2024-03-18 17:15:22 -0300

    feat: config axios api

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosRequestHeaders } from 'axios';
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3001/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem('@Auth:access_token');
+    if (token) {
+      config.headers = {
+        Authorization: `Bearer ${token}`,
+      } as AxiosRequestHeaders;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  },
+);
