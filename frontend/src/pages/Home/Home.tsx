4d54d0580233fb637f24bcc194e29327cf09d52e
rafael <rafaelmagalhaes1@live.com>
2024-03-18 18:11:31 -0300

    feat: Home page created

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { Posts } from '../../components/Posts/Posts';
+import { Container } from './Style';
+
+export function Home() {
+  return (
+    <Container>
+      <Posts />
+    </Container>
+  );
+}
