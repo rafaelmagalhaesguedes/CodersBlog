50bf6916557ec14c4c6ecfcb71451cb1cff1c28b
rafael <rafaelmagalhaes1@live.com>
2024-03-18 17:20:15 -0300

    feat: create routes/ProtectRoutes

---
+++ frontend/src/routes/ProtectRoutes.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from '../components/Layout/Layout';
+import { Home } from '../pages/Home/Home';
+
+export function ProtectRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Layout /> }>
+          <Route path="/" element={ <Home /> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
