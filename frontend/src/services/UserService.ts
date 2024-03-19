4fc6ed6217a68c94db126d808299a26bf68dca18
rafael <rafaelmagalhaes1@live.com>
2024-03-19 11:16:04 -0300

    feat: UserService created

---
+++ frontend/src/services/UserService.ts
@@ -0,0 +1,61 @@
+import { UserType } from '../types/UserType';
+import { HOST } from './ApiService';
+
+const fetchWithAuth = async (url: string, options: RequestInit) => {
+  //
+  const token = localStorage.getItem('@Auth:access_token');
+
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+    ...options.headers,
+  };
+
+  const response = await fetch(url, { ...options, headers });
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} ${response.statusText}`);
+  }
+
+  return response.json();
+};
+
+export const registerUser = async (userData: UserType) => {
+  //
+  const { username, email, password, role, image } = userData;
+
+  const data = await fetchWithAuth(`${HOST}/user`, {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+      role,
+      image,
+    }),
+  });
+
+  localStorage.setItem('@Auth:access_token', data.token);
+
+  return data;
+};
+
+export const getUserPosts = async (id: string) => {
+  return fetchWithAuth(`${HOST}/post/user/${id}`, { method: 'GET' });
+};
+
+export const editUserPost = async (postId: number, editingPostData: UserType) => {
+  //
+  await fetchWithAuth(`${HOST}/post/${postId}`, {
+    method: 'PUT',
+    body: JSON.stringify(editingPostData),
+  });
+
+  return true;
+};
+
+export const deleteUserPost = async (postId: number) => {
+  await fetchWithAuth(`${HOST}/post/${postId}`, { method: 'DELETE' });
+
+  return true;
+};
