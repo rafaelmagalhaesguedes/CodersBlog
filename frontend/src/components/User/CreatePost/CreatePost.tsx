9b1a004e569aa1f755cc2845f504172834682c2b
rafael <rafaelmagalhaes1@live.com>
2024-03-19 21:39:42 -0300

    feat: component CreatePost created

---
+++ frontend/src/components/User/CreatePost/CreatePost.tsx
@@ -0,0 +1,71 @@
+import { useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { FaArrowLeft, FaBookOpen } from 'react-icons/fa';
+import { usePost } from '../../../hooks/usePost';
+import { CreatePostButton, CreatePostContainer } from './Style';
+
+export function CreatePost() {
+  const { id } = useParams();
+  const {
+    title,
+    setTitle,
+    content,
+    setContent,
+    categories,
+    selectedCategories,
+    fetchCategories,
+    handleCreatePost,
+    handleCheckboxChange,
+  } = usePost(Number(id));
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  return (
+    <CreatePostContainer>
+      <div>
+        <Link to="/">
+          <FaArrowLeft size={ 15 } />
+          Back
+        </Link>
+        <h3>Create new post</h3>
+        <Link to="/user-posts">
+          <span>
+            <p>My posts</p>
+            <FaBookOpen />
+          </span>
+        </Link>
+      </div>
+      <input
+        type="text"
+        placeholder="Title"
+        value={ title }
+        onChange={ (e) => setTitle(e.target.value) }
+      />
+      <textarea
+        placeholder="Content"
+        value={ content }
+        onChange={ (e) => setContent(e.target.value) }
+      />
+      <div className="title-categories">
+        <h3>Categories</h3>
+      </div>
+      <ul>
+        {categories.map((category: any) => (
+          <li key={ category.id }>
+            <input
+              type="checkbox"
+              id={ `category-${category.id}` }
+              value={ category.id }
+              checked={ selectedCategories.includes(category.id) }
+              onChange={ handleCheckboxChange }
+            />
+            <label htmlFor={ `category-${category.id}` }>{category.name}</label>
+          </li>
+        ))}
+      </ul>
+      <CreatePostButton onClick={ handleCreatePost }>Create Post</CreatePostButton>
+    </CreatePostContainer>
+  );
+}
---
+++ frontend/src/components/User/CreatePost/Style.tsx
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+
+export const CreatePostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  width: 80%;
+  margin: 0 auto;
+  padding: 30px 50px;
+  gap: 5px;
+  background-color: #fff;
+  border-top: 1px solid #ccc;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 5px;
+    background-color: #f2f2f2;
+    padding: 10px;
+    border-radius: 5px;
+  }
+
+  span {
+      display: flex;
+      align-items: center;
+      gap: 7px;
+      cursor: pointer;
+      font-size: 16px;
+  }
+
+  a {
+    border-radius: 5px;
+    text-decoration: none;
+    color: #000;
+    font-weight: 400;
+  }
+
+  .title-categories {
+    margin-top: 20px;
+
+    h3 {
+      margin: 0;
+      padding: 0;
+    }
+  }
+
+  input, select, textarea {
+    width: 100%;
+    padding: 10px;
+    margin-top: 10px;
+    cursor: pointer;
+  }
+  
+  textarea {
+    height: 200px;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  li {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 5px;
+  }
+
+  label {
+    margin: 0;
+    padding: 0;
+    margin-top: 5px;
+    cursor: pointer;
+  }
+
+  input[type="checkbox"] {
+    cursor: pointer;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export const CreatePostButton = styled.button`
+  margin-top: 10px;
+  width: 100%;
+  padding: 10px;
+  border: none;
+  background-color: #000;
+  color: #fff;
+  cursor: pointer;
+  font-size: 15px;
+`;
