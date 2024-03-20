c9ced02bb4674e494cb6d5d4aeeada3eee925a22
rafael <rafaelmagalhaes1@live.com>
2024-03-20 09:22:55 -0300

    feat: add new routes

--- frontend/src/routes/ProtectRoutes.tsx
+++ frontend/src/routes/ProtectRoutes.tsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from '../components/Layout/Layout';
 import { Home } from '../pages/Home/Home';
+import { SinglePost } from '../components/Posts/SinglePost/SinglePost';
+import { EditPost } from '../components/User/EditPost/EditPost';
+import { CreatePost } from '../components/User/CreatePost/CreatePost';
+import { Categories } from '../components/Categories/Categories';
+import { UserPosts } from '../components/User/UserPost/UserPost';
 
 export function ProtectRoutes() {
   return (
@@ -8,6 +13,12 @@ export function ProtectRoutes() {
       <Routes>
         <Route path="/" element={ <Layout /> }>
           <Route path="/" element={ <Home /> } />
+          <Route path="/single-post/:id" element={ <SinglePost /> } />
+          <Route path="/edit-post/:id" element={ <EditPost /> } />
+          <Route path="/create-post" element={ <CreatePost /> } />
+          <Route path="/categories" element={ <Categories /> } />
+          <Route path="/user-posts" element={ <UserPosts /> } />
+          <Route path="*" element={ <h1>Page Not Found</h1> } />
         </Route>
       </Routes>
     </BrowserRouter>
