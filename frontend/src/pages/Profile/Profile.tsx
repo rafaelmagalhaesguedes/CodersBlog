de0494fa60a6931877a09dfefae50ec8d3f1ad0b
rafael <rafaelmagalhaes1@live.com>
2024-03-20 09:19:35 -0300

    feat: ProfilePage created

---
+++ frontend/src/pages/Profile/Profile.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable no-nested-ternary */
+import { Link } from 'react-router-dom';
+import { FaUserAlt } from 'react-icons/fa';
+import { useProfile } from '../../hooks/useProfile';
+import { ProfileContainer } from './Style';
+
+export function Profile() {
+  const {
+    editing,
+    username,
+    setUsername,
+    email,
+    setEmail,
+    image,
+    setImage,
+    handleLogout,
+    handleEdit,
+  } = useProfile();
+
+  return (
+    <ProfileContainer>
+      <div>
+        <h2>Profile</h2>
+        {editing ? (
+          <input value={ image || '' } onChange={ (e) => setImage(e.target.value) } />
+        ) : (
+          image ? (
+            <img src={ image } alt={ username } />
+          ) : (
+            <FaUserAlt size={ 100 } />
+          )
+        )}
+        <p>
+          <strong>
+            User:
+          </strong>
+          {editing
+            ? <input
+                value={ username }
+                onChange={ (e) => setUsername(e.target.value) }
+            /> : username }
+        </p>
+        <p>
+          <strong>
+            Email:
+          </strong>
+          {editing
+            ? <input
+                value={ email }
+                onChange={ (e) => setEmail(e.target.value) }
+            /> : email }
+        </p>
+
+        <button onClick={ handleEdit }>{editing ? 'Save' : 'Edit Profile'}</button>
+        <Link to="/" onClick={ handleLogout }>Logout</Link>
+        <Link to="/">Back to home</Link>
+      </div>
+    </ProfileContainer>
+  );
+}
