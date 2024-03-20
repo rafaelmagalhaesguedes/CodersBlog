99dc0a362cee4144528f6d028f0950298b2b856c
rafael <rafaelmagalhaes1@live.com>
2024-03-19 21:40:07 -0300

    refact: PostService

--- frontend/src/services/PostService.ts
+++ frontend/src/services/PostService.ts
@@ -1,25 +1,74 @@
 import { PostType } from '../types/PostType';
+import { HOST } from './ApiService';
 
-export const findAll = async () => {
-  const res = await fetch('http://localhost:3001/post', {
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${localStorage.getItem('@Auth:access_token')}`,
+};
+
+export const createPost = async (
+  userId: number,
+  title: string,
+  content: string,
+  categoryIds: number[],
+) => {
+  const res = await fetch(`${HOST}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      id: userId,
+      title,
+      content,
+      categoryIds,
+    }),
+  });
+
+  if (res.status !== 201) {
+    const data = await res.json();
+    throw new Error(data.message);
+  }
+
+  return res.json();
+};
+
+export const findAllPosts = async () => {
+  const res = await fetch(`${HOST}`, {
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('@Auth:access_token')}`,
-    },
+    headers,
   });
   let data = await res.json();
   data = data.sort((a: PostType, b: PostType) => b.id - a.id);
   return data;
 };
 
-export const searchPost = async (searchQuery: string) => {
-  const res = await fetch(`http://localhost:3001/post/search?q=${searchQuery}`, {
+export const findPostById = async (id: number) => {
+  const res = await fetch(`${HOST}/${id}`, {
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('@Auth:access_token')}`,
-    },
+    headers,
+  });
+  const data = await res.json();
+  return data;
+};
+
+export const updatePost = async (postId: number, title: string, content: string) => {
+  const data = { title, content };
+
+  const response = await fetch(`${HOST}/${postId}`, {
+    method: 'PUT',
+    headers,
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    return true;
+  }
+  throw new Error('Failed to update post');
+};
+
+export const searchPost = async (searchQuery: string) => {
+  const res = await fetch(`${HOST}/search?q=${searchQuery}`, {
+    method: 'GET',
+    headers,
   });
   let data = await res.json();
   data = data.sort((a: PostType, b: PostType) => b.id - a.id);
