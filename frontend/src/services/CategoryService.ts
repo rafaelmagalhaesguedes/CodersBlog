43ef1d924a4c9ec36549410cb30caefeda6ad2a4
rafael <rafaelmagalhaes1@live.com>
2024-03-19 21:48:01 -0300

    feat: CategoryService created

---
+++ frontend/src/services/CategoryService.ts
@@ -0,0 +1,63 @@
+import { HOST } from './ApiService';
+
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${localStorage.getItem('@Auth:access_token')}`,
+};
+
+export const createCategory = async (name: string) => {
+  const res = await fetch(`${HOST}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      name,
+    }),
+  });
+
+  if (res.status !== 201) {
+    const data = await res.json();
+    throw new Error(data.message);
+  }
+
+  return res.json();
+};
+
+export const getCategories = async () => {
+  const res = await fetch(`${HOST}`, {
+    method: 'GET',
+    headers,
+  });
+  const data = await res.json();
+  return data;
+};
+
+export const updateCategory = async (id: number, name: string) => {
+  const res = await fetch(`${HOST}/${id}`, {
+    method: 'PUT',
+    headers,
+    body: JSON.stringify({
+      name,
+    }),
+  });
+
+  if (res.status !== 200) {
+    const data = await res.json();
+    throw new Error(data.message);
+  }
+
+  return res.json();
+};
+
+export const deleteCategory = async (id: string) => {
+  const res = await fetch(`${HOST}/${id}`, {
+    method: 'DELETE',
+    headers,
+  });
+
+  if (res.status !== 200) {
+    const data = await res.json();
+    throw new Error(data.message);
+  }
+
+  return res.json();
+};
