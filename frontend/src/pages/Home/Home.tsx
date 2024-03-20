d75692dbc497bfa2a266a7033277e647f33f16ec
rafael <rafaelmagalhaes1@live.com>
2024-03-19 23:24:34 -0300

    fix: path AllPosts Home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Posts } from '../../components/Posts/Posts';
+import { Posts } from '../../components/Posts/AllPosts.tsx/Posts';
 import { Container } from './Style';
 
 export function Home() {
