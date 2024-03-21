eff3545b0fc4ddb062e526f17c89496b711a7524
rafael <rafaelmagalhaes1@live.com>
2024-03-21 14:29:48 -0300

    fix: authentication

--- frontend/src/context/auth.tsx
+++ frontend/src/context/auth.tsx
@@ -1,4 +1,5 @@
 import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 import { createContext, useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../interfaces/AuthContext';
 import { LoginType } from '../types/LoginType';
@@ -9,6 +10,7 @@ const Auth = createContext<AuthContext>({} as AuthContext);
 function AuthProvider({ children }: any) {
   const [user, setUser] = useState<object | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function loadStorageData() {
@@ -40,13 +42,20 @@ function AuthProvider({ children }: any) {
 
       const loggedInUser = userData.data.find((el: LoginType) => el.email === email);
 
-      if (!loggedInUser) throw new Error('User not found in users array');
+      if (!loggedInUser) {
+        console.log('User not found in users array');
+        throw new Error('User not found in users array');
+      }
 
       setUser(loggedInUser);
 
       localStorage.setItem('@Auth:access_token', res.data.token);
       localStorage.setItem('@Auth:user', JSON.stringify(loggedInUser));
+
+      navigate('/');
     } catch (err) {
+      const newError = err as Error;
+      console.log('Error login: ', newError.message);
       console.error('Error login: ', err);
       Swal.fire({
         icon: 'error',
