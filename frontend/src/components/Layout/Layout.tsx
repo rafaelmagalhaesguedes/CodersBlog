b8043ef56044f0b20baa33326796bc52f29ea689
rafael <rafaelmagalhaes1@live.com>
2024-03-21 14:29:16 -0300

    fix: Layout

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '../Header/Header';
+import { CustomHeader } from '../Header/CustonHeader';
 
 export function Layout() {
   return (
     <>
-      <Header />
+      <CustomHeader />
       <Outlet />
     </>
   );
