5f2b332aef3a12afc16b6f35ca9fb0dc2014b599
rafael <rafaelmagalhaes1@live.com>
2024-03-23 08:39:21 -0300

    feat: CasheService created

--- app/frontend/src/services/CasheService.ts
+++ app/frontend/src/services/CasheService.ts
@@ -1,6 +1,7 @@
 let cache: any = null;
 let cacheTimestamp: number = 0;
 const CACHE_VALIDITY = 5 * 60 * 1000; // Cache is valid for 5 minutes
+const denyList: string[] = []; // List of denied tokens
 
 export const getCache = () => {
   const currentTime = Date.now();
@@ -18,4 +19,17 @@ export const setCache = (data: any) => {
   // Update cache and timestamp
   cache = data;
   cacheTimestamp = Date.now();
+};
+
+export const clearCache = () => {
+  cache = null;
+  cacheTimestamp = 0;
+};
+
+export const addToDenyList = (token: string) => {
+  denyList.push(token);
+};
+
+export const isTokenDenied = (token: string) => {
+  return denyList.includes(token);
 };
