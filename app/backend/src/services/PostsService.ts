3137ece3f1af8937f40cdeddf13758cfc830d20f
rafael <rafaelmagalhaes1@live.com>
2024-03-23 08:46:58 -0300

    fix: searchPosts on PostsServices backend

--- app/backend/src/services/PostsService.ts
+++ app/backend/src/services/PostsService.ts
@@ -101,15 +101,11 @@ export default class LoginService {
 
   public async searchPosts(search: string): Promise<ServiceResponse<IPosts[]>> {
     //
-    if (typeof search !== 'string') {
-      return { status: 'INTERNAL_ERROR', data: { message: 'Search query not found!' } };
-    }
-
     if (search === '') return this.getPosts();
 
     const posts = await this.postsModel.search(search);
 
-    if (posts === null) {
+    if (!posts) {
       return { status: 'NOT_FOUND', data: { message: 'Posts not found!' } };
     }
 
