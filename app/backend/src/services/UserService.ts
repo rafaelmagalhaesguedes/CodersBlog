8ed893960d26ba8e65317500233f915e9767ed39
rafael <rafaelmagalhaes1@live.com>
2024-03-24 10:34:04 -0300

    fix: remove pass hash on createUser on servicer

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -15,10 +15,7 @@ export default class UserService {
     if (user) {
       return { status: 'CONFLICT', data: { message: 'User already exists!' } };
     }
-
-    const salt = bcript.genSaltSync(10);
-    data.password = bcript.hashSync(data.password, salt);
-
+    
     const newUser = await this.userModel.create(data);
     if (!newUser) {
       return { status: 'INTERNAL_ERROR', data: { message: 'User not created!' } };
