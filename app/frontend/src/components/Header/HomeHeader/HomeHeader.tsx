81fa9ae0012b4d2e4e1b834a35bd685b7abcb779
rafael <rafaelmagalhaes1@live.com>
2024-03-25 13:20:46 -0300

    style: add mobile style on home header

--- app/frontend/src/components/Header/HomeHeader/HomeHeader.tsx
+++ app/frontend/src/components/Header/HomeHeader/HomeHeader.tsx
@@ -1,19 +1,28 @@
 /* eslint-disable react/jsx-max-depth */
 import { Link } from 'react-router-dom';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { useState } from 'react';
+import { FaArrowRight, FaBars, FaGithub, FaLinkedin } from 'react-icons/fa';
 import LogoImage from '../../../assets/img/logo.png';
 import {
   HeaderContainer,
   MenuTop,
   Logo,
   SignUp,
+  MobileIcon,
+  MobileMenu,
 } from './Style';
 
 export function HomeHeader() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <MenuTop>
-        <span>Web Technology Blog</span>
+        <span>Blogs API</span>
         <div>
           <Link to="https://github.com/rafaelmagalhaesguedes" target="_blank">
             <FaGithub size={ 20 } color="#fff" />
@@ -33,6 +42,28 @@ export function HomeHeader() {
           <Link to="/login">Login</Link>
           <Link to="/register">Register</Link>
         </SignUp>
+        <MobileIcon onClick={ handleToggle }>
+          <FaBars />
+        </MobileIcon>
+        {isOpen && (
+          <MobileMenu>
+            <Link to="/" onClick={ handleToggle }>
+              <FaArrowRight size={ 12 } />
+              {' '}
+              Posts
+            </Link>
+            <Link to="/login" onClick={ handleToggle }>
+              <FaArrowRight size={ 12 } />
+              {' '}
+              Login
+            </Link>
+            <Link to="/register" onClick={ handleToggle }>
+              <FaArrowRight size={ 12 } />
+              {' '}
+              Register
+            </Link>
+          </MobileMenu>
+        )}
       </HeaderContainer>
     </>
   );
--- app/frontend/src/components/Header/HomeHeader/Style.tsx
+++ app/frontend/src/components/Header/HomeHeader/Style.tsx
@@ -20,7 +20,6 @@ export const MenuTop = styled.div`
 
   div {
     display: flex;
-    gap: 10px;
   }
 
   span {
@@ -29,6 +28,10 @@ export const MenuTop = styled.div`
     padding: 0 10px;
     letter-spacing: 1px;
     font-weight: 500;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
   }
 `;
 
@@ -42,8 +45,9 @@ export const HeaderContainer = styled.header`
   margin: 0 auto;
   padding: 0 20px;
 
-  @media (max-width: 1000px) {
+  @media (max-width: 768px) {
     justify-content: space-between;
+    width: 100%;
   }
 `;
 
@@ -64,6 +68,20 @@ export const Logo = styled.div`
     font-family: 'Rubik Dirt';
     letter-spacing: 1px;
   }
+
+  @media (max-width: 768px) {
+    margin-top: 0;
+    
+    img {
+      width: 60px;
+    }
+
+    span {
+      padding-top: 10px;
+      width: 130px;
+      font-size: 1.5rem;
+    }
+  }
 `;
 
 export const SignUp = styled.div`
@@ -76,6 +94,46 @@ export const SignUp = styled.div`
   a {
     text-decoration: none;
     color: #000;
+    font-size: 1rem;
+    font-weight: 500;
+    letter-spacing: 1px;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+    font-size: 2rem;
+    cursor: pointer;
+  }
+`;
+
+export const MobileMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  justify-content: center;
+  gap: 2rem;
+  position: fixed;
+  top: 80px;
+  right: 0;
+  width: 50%;
+  height: 40vh;
+  background-color: #1a1b1c;
+  color: #fff;
+  z-index: 999;
+  transition: 0.3s ease-in-out;
+  border-radius: 0 20px 0 20px;
+
+  a {
+    text-decoration: none;
+    color: #fff;
     font-size: 1rem;
     font-weight: 500;
     letter-spacing: 1px;
